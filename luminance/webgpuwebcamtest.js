972878580851f8c4285abc9600f00f241971abcd
Anthony Gagnon <59715170+dany-demise@users.noreply.github.com>
2024-09-24 19:38:18 -0400

    Update webgpuwebcamtest.js

--- luminance/webgpuwebcamtest.js
+++ luminance/webgpuwebcamtest.js
@@ -110,7 +110,7 @@ fn vertex_main(@location(0) position: vec4f,
 fn fragment_main(fragData: VertexOut) -> @location(0) vec4f
 {
     let sampledColor = textureSampleBaseClampToEdge(myTexture, mySampler, fragData.fragUV);
-    return vec4f(sampledColor.r, sampledColor.g * 0.5, sampledColor.b, sampledColor.a);
+    return vec4f(sampledColor.y, sampledColor.y, sampledColor.y, sampledColor.a);
 }
 
 `;
