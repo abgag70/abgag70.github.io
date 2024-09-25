29adee009871fd01781cc03eeddf3c2b627d421f
Anthony Gagnon <59715170+dany-demise@users.noreply.github.com>
2024-09-25 13:07:02 -0400

    Update webgpuwebcamtest.js

--- luminance/webgpuwebcamtest.js
+++ luminance/webgpuwebcamtest.js
@@ -159,7 +159,7 @@ fn fragment_main(fragData: VertexOut) -> @location(0) vec4f
     // return fragData.color;
 
     let sampledColor = textureSampleBaseClampToEdge(myTexture, mySampler, fragData.fragUV);
-    let luminance_gamma_corrected = pow((sampledColor.y - 1.0) * -1.0, 0.454545); // pow(sampledColor.y, 2.2);
+    let luminance_gamma_corrected = pow((sampledColor.y - 1.0) * -1.0, 1.0); // pow(sampledColor.y, 2.2);
     let clamped_luminance = clamp(luminance_gamma_corrected * 255.0, 0.0, 255.0);
     let index = i32(clamped_luminance);
 
